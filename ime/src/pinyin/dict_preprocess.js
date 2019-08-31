b5ac43624cedb53ec3b48607029e6174e20898a4
Dong Yuwei <newdongyuwei@gmail.com>
2019-08-31 08:44:09 +0800

    Revert "delete the original dict material and preprocess script (but keep them in dev branch)"
    This reverts commit 945aca9bf8c5dfb85a107112e807d287a6b93baf.

---
+++ ime/src/pinyin/dict_preprocess.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+// iconv -f UTF-16 -t UTF-8 google_pinyin_rawdict_utf16_65105_freq.txt  > google_pinyin_rawdict_utf8_65105_freq.txt
+
+const googlePinyin = fs.readFileSync('./google_pinyin_rawdict_utf8_65105_freq.txt', 'utf-8');
+const lines = googlePinyin
+  .split('\n')
+  .filter(line => {
+    return line.includes(' 0 '); //filter ' 1 ', which is 繁体
+  })
+  .map(function(line) {
+    return line.replace(' 0 ', '|');
+  });
+console.log(lines.length);
+const content = `module.exports = "${lines.join(';')}"`;
+fs.writeFileSync('google_pinyin_dict_utf8_55320.js', content, 'utf-8');
