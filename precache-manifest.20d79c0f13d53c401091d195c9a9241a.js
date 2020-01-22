c504349d4715888da7293e2d00187b2985a6462b
Dong Yuwei <newdongyuwei@gmail.com>
2020-01-22 17:31:10 +0800

    Updates

---
+++ precache-manifest.20d79c0f13d53c401091d195c9a9241a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822eb531deb9953e040818ff5c2363fb",
+    "url": "/web-pinyin-ime/index.html"
+  },
+  {
+    "revision": "9fec95e311fe14bbae22",
+    "url": "/web-pinyin-ime/static/css/2.bd6a9626.chunk.css"
+  },
+  {
+    "revision": "d35daa388637dfa0df0d",
+    "url": "/web-pinyin-ime/static/css/main.84f05084.chunk.css"
+  },
+  {
+    "revision": "9fec95e311fe14bbae22",
+    "url": "/web-pinyin-ime/static/js/2.a9954ce9.chunk.js"
+  },
+  {
+    "revision": "d35daa388637dfa0df0d",
+    "url": "/web-pinyin-ime/static/js/main.3cf07258.chunk.js"
+  },
+  {
+    "revision": "375dca1b6838ed70a66b",
+    "url": "/web-pinyin-ime/static/js/runtime~main.9a572d85.js"
+  }
+]);
