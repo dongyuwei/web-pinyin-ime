2b3a5f655dd6fbc887b33af908fb224d936e9526
Dong Yuwei <newdongyuwei@gmail.com>
2022-04-07 14:47:05 +0800

    pre-build dict

--- ime/apps/ime/src/pinyin/ime_engine.js
+++ ime/apps/ime/src/pinyin/ime_engine.js
@@ -1,41 +1,8 @@
 import { flatten, uniq } from 'lodash';
-import { words, packedTrie } from './google_pinyin_dict_utf8_55320.js';
+import { dict, packedTrie } from './google_pinyin_dict_utf8_55320.js';
 const PTrie = require('dawg-lookup/lib/ptrie').PTrie;
 
-const buildDict = words => {
-  const dict = {};
-  words.split(';').forEach(line => {
-    // 帮 30125.3295903|bang
-    // 西红柿 760.851466162|xi hong shi
-
-    const arr = line.split('|');
-    if (arr.length === 2) {
-      //abbr is the first chars of the pinyin, eg: `xhs` is abbr of `xi hong shi`
-      const abbr = arr[1]
-        .split(' ')
-        .map(item => item.substring(0, 1))
-        .join('');
-
-      const pinyin = arr[1].replace(/\s/g, '');
-      const [word, frequency] = arr[0].split(' ');
-
-      const value = {
-        w: word,
-        f: parseFloat(frequency)
-      };
-      dict[pinyin] = dict[pinyin] || [];
-      dict[pinyin].push(value);
-      if (abbr.length >= 2) {
-        dict[abbr] = dict[abbr] || [];
-        dict[abbr].push(value);
-      }
-    }
-  });
-
-  return dict;
-};
-
-const getCandidates = (trie, dict) => input => {
+const getCandidates = (trie, dict) => (input) => {
   let list = [];
   if (input) {
     const value = dict[input];
@@ -44,16 +11,16 @@ const getCandidates = (trie, dict) => input => {
       list = dict[input];
     } else if (input.length > 2) {
       // pinyin prefix match, using prepared packed trie data.
-      list = flatten(trie.completions(input).map(key => dict[key]));
+      list = flatten(trie.completions(input).map((key) => dict[key]));
     }
 
     //sort candidates by word frequency
     list = list
-    .filter(item => !!item)
-    .sort((a, b) => b.f - a.f)
-    .map(item => item.w);
+      .filter((item) => !!item)
+      .sort((a, b) => b.f - a.f)
+      .map((item) => item.w);
   }
   return uniq(list);
 };
 
-export default getCandidates(new PTrie(packedTrie), buildDict(words));
+export default getCandidates(new PTrie(packedTrie), dict);
--- ime/apps/ime/src/script/dict_preprocess.js
+++ ime/apps/ime/src/script/dict_preprocess.js
@@ -3,14 +3,17 @@ var Trie = require('dawg-lookup').Trie;
 
 // iconv -f UTF-16 -t UTF-8 rawdict_utf16_65105_freq.txt  > google_pinyin_rawdict_utf8_65105_freq.txt
 
-const googlePinyin = fs.readFileSync('./google_pinyin_rawdict_utf8_65105_freq.txt', 'utf-8');
+const googlePinyin = fs.readFileSync(
+  './google_pinyin_rawdict_utf8_65105_freq.txt',
+  'utf-8'
+);
 const pinyinList = [];
 const lines = googlePinyin
   .split('\n')
-  .filter(line => {
+  .filter((line) => {
     return line.includes(' 0 '); // ' 0 ' means simplified chinese characters', while ' 1 ' means traditional chinese characters.
   })
-  .map(function(line) {
+  .map(function (line) {
     const pinyin = line.split(' 0 ')[1].replace(/\s/g, '');
     pinyinList.push(pinyin);
     return line.replace(' 0 ', '|');
@@ -18,10 +21,48 @@ const lines = googlePinyin
 
 console.log('total words:', lines.length);
 
+const buildDict = () => {
+  const dict = {};
+  lines.forEach((line) => {
+    // 帮 30125.3295903|bang
+    // 西红柿 760.851466162|xi hong shi
+
+    const arr = line.split('|');
+    if (arr.length === 2) {
+      //abbr is the first chars of the pinyin, eg: `xhs` is abbr of `xi hong shi`
+      const abbr = arr[1]
+        .split(' ')
+        .map((item) => item.substring(0, 1))
+        .join('');
+
+      const pinyin = arr[1].replace(/\s/g, '');
+      const [word, frequency] = arr[0].split(' ');
+
+      const value = {
+        w: word,
+        f: parseFloat(frequency),
+      };
+      dict[pinyin] = dict[pinyin] || [];
+      dict[pinyin].push(value);
+      if (abbr.length >= 2) {
+        dict[abbr] = dict[abbr] || [];
+        dict[abbr].push(value);
+      }
+    }
+  });
+
+  return dict;
+};
+
 const trie = new Trie(pinyinList.join(' '));
 
 const content = `
-  exports.words = "${lines.join(';')}";
+  exports.dict = ${JSON.stringify(buildDict())};
   exports.packedTrie = "${trie.pack()}";
 `;
-fs.writeFileSync('../pinyin/google_pinyin_dict_utf8_55320.js', content, 'utf-8');
+
+fs.writeFileSync(
+  '../pinyin/google_pinyin_dict_utf8_55320.js',
+  content,
+  'utf-8'
+);
